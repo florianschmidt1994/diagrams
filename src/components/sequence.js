ea26626f449ba1a965a3089a0b05900c3315abc4
Florian Schmidt <florian.schmidt.1994@icloud.com>
2022-07-17 22:42:55 +0200

    Improve syntax highlighting for sequence diagrams

--- src/components/sequence.js
+++ src/components/sequence.js
@@ -1,7 +1,29 @@
+const arrows = new RegExp(
+  [/->/, /-->/, /->>/, /-->>/, /-x/, /--x/, /-\)/, /--\)/]
+    .map((r) => r.source)
+    .join("|")
+);
+
+const arrowsWithActivation = new RegExp(`(${arrows.source})[\\+-]?`);
+console.log(arrowsWithActivation);
+
+const leftParticipant = new RegExp(
+  `(\\w+)(?=(${arrowsWithActivation.source}))`
+);
+const rightParticipant = new RegExp(
+  `(?<=(${arrowsWithActivation.source}))(\\w+)`
+);
+
 export default {
   keyword: {
     pattern:
-      /sequenceDiagram|alt|opt|end|else|participant|actor|loop|activate|deactivate|Note|over|right of|left of|critical/,
+      /(?<=(\s|^))(sequenceDiagram|alt|opt|end|par|else|participant|actor|loop|activate|deactivate|Note|over|right of|left of|critical)(?=(\s|$))/,
+  },
+  leftParticipant: {
+    pattern: leftParticipant,
+  },
+  rightParticipant: {
+    pattern: rightParticipant,
   },
   operator: {
     pattern:
